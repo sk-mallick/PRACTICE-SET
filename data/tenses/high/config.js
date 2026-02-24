35354cc091b3c4616655845c623f6d5497028d45
Subham Kumar Mallick <83875827+sk-mallick@users.noreply.github.com>
2026-02-24 06:02:41 +0000

    Update config.js to reflect correct number of sets and questions

--- data/tenses/high/config.js
+++ data/tenses/high/config.js
@@ -1,10 +1,10 @@
 export default {
     order: 3,                    // The number shown on the card (01, 02...)
     title: "Tenses",  // The big bold title
-    description: "6 Sets • 10 Questions Each - H", // The small text below
+    description: "22 Sets • 20 Questions Each - H", // The small text below
     engine: "mcq",               // "mcq" or "fill" (which HTML file to open)
     icon: "book",                 // "time", "list", "book", or "chat"
-    sets: 3,                     // Total number of setN.json files
+    sets: 22,                     // Total number of setN.json files
 
     headerTitle: "TIME & TENSE PRACTICE",                 // Top left title in the engine
     headerSubtitlePrefix: "By Chiranjibi Sir"       // Text before the bullet point (•)
