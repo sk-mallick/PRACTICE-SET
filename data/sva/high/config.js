d842255f74adfefd1153d1354c217c23eb45a759
Subham Kumar Mallick <83875827+sk-mallick@users.noreply.github.com>
2026-02-25 21:41:32 +0530

    Update config.js

--- data/sva/high/config.js
+++ data/sva/high/config.js
@@ -1,7 +1,7 @@
 export default {
     order: 7,                    // The number shown on the card (01, 02...)
     title: "Subject-Verb Agreement",  // The big bold title
-    description: "10 Sets • 215 Questions Each - H", // The small text below
+    description: "10 Sets • 25 Questions Each - H", // The small text below
     engine: "mcq",               // "mcq" or "fill" (which HTML file to open)
     icon: "list",                 // "time", "list", "book", or "chat"
     sets: 10,                    // Total number of setN.json files
@@ -9,4 +9,5 @@ export default {
     headerTitle: "SYNTAX PRACTICE",                 // Top left title in the engine
     headerSubtitlePrefix: "By Chiranjibi Sir",       // Text before the bullet point (•)
     pdfheader: "Subject-Verb Agreement"
+
 }
