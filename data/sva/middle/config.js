f2fa069631757762329fe788baf2fbbd3509577a
Subham Kumar Mallick <83875827+sk-mallick@users.noreply.github.com>
2026-02-25 03:09:45 +0000

    Add pdfheader for Subject-Verb Agreement and Time & Tense in config files

--- data/sva/middle/config.js
+++ data/sva/middle/config.js
@@ -7,5 +7,6 @@ export default {
     sets: 4,                     // Total number of setN.json files
 
     headerTitle: "SYNTAX PRACTICE",                 // Top left title in the engine
-    headerSubtitlePrefix: "By Chiranjibi Sir"       // Text before the bullet point (•)
+    headerSubtitlePrefix: "By Chiranjibi Sir",       // Text before the bullet point (•)
+    pdfheader: "Subject-Verb Agreement"
 }
--- data/tenses/primary/config.js
+++ data/tenses/primary/config.js
@@ -7,5 +7,6 @@ export default {
     sets: 4,                     // Total number of setN.json files
 
     headerTitle: "TIME & TENSE PRACTICE",                 // Top left title in the engine
-    headerSubtitlePrefix: "By Chiranjibi Sir"       // Text before the bullet point (•)
+    headerSubtitlePrefix: "By Chiranjibi Sir",       // Text before the bullet point (•)
+    pdfheader: "Time & Tense"
 }
